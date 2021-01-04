48c023127c95c04c88f0fcd555ae94a0ebd66a9d
akrulcic <akrulcic@users.noreply.github.com>
2021-01-04 20:48:04 +0100

    card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,49 @@
 <template>
-<div class="container">
+<div class="container" id="home">
   <div class="row">
-    <div class="col-2">
-     
-    </div>
-    <div class="col-8">
-      
-    </div>
-    <div class="col-2">
+    <div class="col-1"></div>
+
+    <div class="col-10">
+      <br>
+      <br>
+      <h3>Preporuke</h3>
+      <hr>
+      <Card />
 
     </div>
+    <div class="col-1"></div>
   </div>
 </div>
+
 </template>
+
+
+<style lang="scss">
+#home{
+  h3{
+    text-align: left;
+  }
+}
+#card{
+  text-align: left;
+  font-size: 13px;
+  font-weight: bold;
+  h6{
+    color:rgb(153, 117, 92);
+    font-weight: bold;
+    
+  }
+}
+</style>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
